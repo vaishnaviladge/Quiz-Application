55bf78ba5dcd1c83b3e610746a8f13f37c747373
vaishnaviladge <124393233+vaishnaviladge@users.noreply.github.com>
2023-02-09 00:20:00 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is correct about features of JavaScript?",
+    answer: "All of the above.",
+    options: [
+      "JavaScript is a lightweight, interpreted programming language",
+      "JavaScript is designed for creating network-centric applications",
+      "JavaScript is complementary to and integrated with Java.",
+      "All of the above."
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which of the following function of String object returns the characters in a string between two indexes into the string?",
+    answer: "substring()",
+    options: [
+      "slice()",
+      "split()",
+      "substr()",
+      "substring()"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which of the following function of String object creates an HTML anchor that is used as a hypertext target?",
+    answer: "anchor()",
+    options: [
+      "anchor()",
+      "link()",
+      "blink()",
+      "big()"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which of the following function of Array object represents the source code of an object?",
+    answer: "toSource()",
+    options: [
+      "toSource()",
+      "splice()",
+      "toString()",
+      "unshift()"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which built-in method returns the calling string value converted to lower case?",
+    answer: "toLowerCase()",
+    options: [
+      "toLowerCase()",
+      "toLower()",
+      "changeCase(case)",
+      "None of the above."
+    ]
+  },
+   {
+     numb: 6,
+     question: "Which built-in method returns the length of the string?",
+     answer: "length()",
+     options: [
+       "length()",
+       "size()",
+       "index()",
+       "None of the above."
+     ]
+   },
+   {
+     numb: 7,
+     question: "Which of the following function of String object combines the text of two strings and returns a new string?",
+     answer: "concat()",
+     options: [
+       "add()",
+       " merge()",
+       "concat()",
+       "append()"
+     ]
+   },
+   {
+     numb: 8,
+     question: "Which of the following function of String object causes a string to be displayed as a subscript, as if it were in a <sub> tag?",
+     answer: "sub()",
+     options: [
+       "sup()",
+       "small()",
+       "strike()",
+       "sub()"
+     ]
+    },
+   {
+     numb: 9,
+     question: "Which of the following function of String object causes a string to be displayed as a subscript, as if it were in a <sub> tag?",
+     answer: "join()",
+     options: [
+       "concat()",
+       "join()",
+       "pop()",
+       "map()"
+     ]
+   },
+   {
+     numb: 10,
+     question: "Which built-in method reverses the order of the elements of an array?",
+     answer: "",
+     options: [
+       "changeOrder(order)",
+       "reverse()",
+       "sort(order)",
+       "None of the above."
+     ]
+  },
+
+];
